2072a801094f4bfa28d53e3ad64dbd34137b2cdb
thientran2606 <hieuthien499@gmail.com>
2024-09-19 12:57:25 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const ApiError = require("./app/api-error");
 const cors = require("cors");
-
+const contactsRouter = require("./app/routes/contact.route");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter);
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
 
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to contact book application." });
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
 });
 
 module.exports = app;
